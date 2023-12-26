8e5249f8990d09f7b714ccbcbdd58ba1389a30d6
dwbessa <danielbessa01@gmail.com>
2023-12-26 11:56:00 -0300

    add: aula #22

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,22 +1,29 @@
+import Input from "../../form/Input"
+import Select from "../../form/Select"
+import SubmitButton from "../../form/SubmitButton"
+
 import styles from "./ProjectForm.module.css"
 
-function ProjectForm () {
+function ProjectForm ({ btnText }) {
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do Projeto" />
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total" />
-            </div>
-            <div>
-                <select name="category_id">
-                    <option disabled>Selecione a categoria</option>
-                </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar Projeto" />
-            </div>
+        <form className={styles.form}>
+            <Input 
+                type="text"
+                text="Nome do Projeto"
+                name="name"
+                placeholder="Insira o nome do projeto"
+            />
+            <Input 
+                type="number"
+                text="Insira o Orçamento do Projeto"
+                name="name"
+                placeholder="Insira o orçamento total"
+            />
+            <Select 
+                name="category_id" 
+                text="Selecione a Categoria" 
+            />
+            <SubmitButton text={btnText}/>
     </form>
     )
 }
